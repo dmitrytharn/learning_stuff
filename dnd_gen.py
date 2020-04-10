4f9613394eb00a033e1c981f992ff2c9e60a133b
Dmytro Tharn <inle.tharn@gmail.com>
2020-04-10 19:22:02 +0200

    Added basic re-roll mechanics to script. Wrapped everything in functions

--- dnd_gen.py
+++ dnd_gen.py
@@ -1,12 +1,12 @@
 from random import randint
 # set the main parameters
 my_char = {
-"Strength" : 0,
-"Dexterity" : 0,
-"Constitution" : 0,
-"Wizdom" : 0,
-"Intelligence" : 0,
-"Charisma" : 0,
+"Str" : 0,
+"Dex" : 0,
+"Con" : 0,
+"Wis" : 0,
+"Int" : 0,
+"Cha" : 0,
 }
 #roll 3 dices for a random number
 def dice_roll():
@@ -17,6 +17,28 @@ def dice_roll():
     return total
 
 # add all values and assign it to parameter
+def re_roll():
+    counter_for_reroll = 3
+    while counter_for_reroll > 0:
+        print(f"\nYou have {counter_for_reroll} re-rolls left. Do you want to reroll \n")
+        re_roll = input("y/n? ")
+
+        if re_roll in ['y', "Y"]:
+            param = input("\nWhich param do you want to re-roll? (Str/Dex/Con/Wis/Int/Cha) \n").capitalize()
+            print(param)
+            for key,value in my_char.items():
+                if key != param:
+                    # print("Checking parameters. Fail")
+                    continue
+                elif key == param:
+                    my_char[key] = dice_roll()
+                    print(f"\nNew value for {param} is {my_char[key]} \n")
+                    print_out()
+                    counter_for_reroll -=1
+
+        elif re_roll in ['n', "N"]:
+            break
+
 
 #generate the character - This actually iterates through dictionary of "my_char & rolls dices for each param"
 def generating():
@@ -24,16 +46,24 @@ def generating():
         my_char[key] = dice_roll()
         # print(my_char[key])
 
-
+def print_out():
     for key, value in my_char.items():
         print(key, "--> ", value )
 
-while True:
-    answer = input("Do you want to generate another character? y/n \n")
-    if answer == 'y':
-        generating()
-    elif answer == 'n':
-        print("See you later, stranger")
-        break
-    else:
-        print("The input should be 'y' or 'n' ")
+def main():
+    while True:
+        answer = input("Do you want to generate another character? y/n \n")
+        if answer == 'y':
+            generating()
+            print_out()
+            re_roll()
+            print(my_char)
+        elif answer == 'n':
+            print("See you later, stranger")
+            break
+        else:
+            print("The input should be 'y' or 'n' ")
+
+main()
+
+#you have 3 re-rolls left.
