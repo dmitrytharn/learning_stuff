3b201026a589008fdacd44cc06e9980708d28be5
Dmytro Tharn <inle.tharn@gmail.com>
2020-04-13 13:47:14 +0200

    Added a way to enter the name for char manually

--- name_gen.py
+++ name_gen.py
@@ -1,6 +1,7 @@
 import sys
 import random
 
+
 # Set the tuples with first name & last name to pick with
 first_name = ("Ficadon","Fickfylo","Fildon","Firedorn","Firiro","Floran","Folmard",'Fraderk','Fronar','Fydar','Fyn')
 last_name = ("Dragonborne", "Stormbreaker", "Mountaintop", "Grimmerwald", "Axechop", "Foldings", "Willingfoe")
@@ -25,9 +26,22 @@ def get_name():
             print("\n")
             new_name = chosen_first + " " + chosen_last
         elif answer == "n":
-            name = "Unnamed character"
-            print("Ok. See you again!")
-            break
+            want_to_name = input("\nDo you want to name the character yourself? y/n ")
+            if want_to_name.lower() == 'y':
+                new_first = input("Enter characters first name: ")
+                if new_first.isalpha():
+                    new_last = input("Enter characters last name: ")
+                    if new_last.isalpha():
+                        new_name = new_first.capitalize() + " " + new_last.capitalize()
+                        print(f"You've chosen this name for the hero: {new_name}")
+                        break
+            elif want_to_name.lower() == "n":
+                name = "Unnamed character"
+                print("Ok. See you again!")
+                break
+            else:
+                print("\nJust 'y' or 'n', please.\n")
+
         elif answer == "n" and new_name:
             name = new_name
             print("Ok. See you again!")
@@ -38,19 +52,6 @@ def get_name():
         return new_name
     except UnboundLocalError:
         return name
-
-
-    #     if answer == "n":
-    #         if len(name) < 3:
-    #             name = "Unnamed character"
-    #         else:
-    #             name =
-    #         print("Ok. See you again!")
-    #         break
-    #     elif answer not in ["y", 'n']:
-    #         print("Just 'y' or 'n' please")
-    # return name
-
 
 
 if __name__ == "__main__":
